70b3cb676e3ccd4a3c9d106cf55c7ff4840228aa
Hicanos <ruda32168@gmail.com>
2025-08-15 18:49:39 +0900

    [Add] UIManager

---
+++ Assets/02. Scripts/Managers/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class UIManager : GlobalSingletonMono<UIManager>
+{
+    private Transform popupUiTransform;
+    private GameObject curUI;      
+
+    public void OpenUI(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    // 단일 PopupUI를 끄는 함수
+    public void CloseUI(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+}
