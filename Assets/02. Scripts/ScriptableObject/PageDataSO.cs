d2609a5c862dfbf51de2b0cf854c3afb7a3d164c
Hyunho <lumenize@ajou.ac.kr>
2025-08-16 07:36:19 +0900

    * Change SO Area

--- Assets/02. Scripts/ScriptableObject/PageDataSO.cs
+++ Assets/02. Scripts/ScriptableObject/PageDataSO.cs
@@ -6,5 +6,6 @@ public class PageDataSO : ScriptableObject
 {
     [SerializeField] private List<Sprite> pieceImgs;
     [SerializeField] private List<Sprite> answerImgs;
+    [TextArea]
     [SerializeField] private string desc;
 }
