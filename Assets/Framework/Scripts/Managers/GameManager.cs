12a405a498087facb8194727b3d7d8edddd1d3f0
Hyunho <lumenize@ajou.ac.kr>
2025-08-15 18:29:17 +0900

    * 시작 및 종료 버튼 제작

--- Assets/Framework/Scripts/Managers/GameManager.cs
+++ Assets/Framework/Scripts/Managers/GameManager.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class GameManager : GlobalSingletonMono<GameManager>
 {
+    #region [ Private Method ]
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+    }
 
+    public void OpenSettingUI()
+    {
+        Debug.Log("OpenSetting UI");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+#endregion [ Private Method ]
 }
