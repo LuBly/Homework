a6ef006d5a405f3cb5e0143eb8cf272d58582d3f
Hyunho <lumenize@ajou.ac.kr>
2025-08-16 02:36:01 +0900

    * hello

---
+++ Assets/02. Scripts/ScriptableObject/PageDataSO.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Page Data", menuName = "Scriptable Object/Page Data")]
+public class PageDataSO : ScriptableObject
+{
+    [SerializeField] private List<Sprite> pieceImgs;
+    [SerializeField] private List<Sprite> answerImgs;
+    [SerializeField] private string desc;
+}
