25ebb6a1e1dce043ae2b6b566f593c4f8a25c821
Hicanos <ruda32168@gmail.com>
2025-08-15 23:09:52 +0900

    ✨ [Feat] 필름 클릭 시 확대
    필름 클릭시 확대(판넬) 구현

--- Assets/02. Scripts/UI/Film.cs
+++ Assets/02. Scripts/UI/Film.cs
@@ -3,7 +3,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 // 드래그 시작시 Film 하위의 이미지 오브젝트를 드래그&드롭
-public class Film : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Film : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
 {
     // 드래그 시작 시: 해당 오브젝트를 SetActive로 비활성화, 마우스 위치에 Prefab을 생성하여 드래그 시작
 
@@ -11,21 +11,27 @@ public class Film : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
     public static GameObject beingDraggImg; // 현재 드래그 중인 이미지 오브젝트
     public Image filmImage; // 드래그 중인 이미지 오브젝트의 이미지 컴포넌트
     private Color imageColor; // 드래그 중인 이미지의 색상 정보
+    [SerializeField] private FilmPanel filmPanel; // 클릭 시 해당 이미지를 확대하여 보여줄 패널
 
     //Film Data라는 이름의 ScriptableObject를 통해 드래그된 이미지 정보 저장/관리
 
     public Vector3 startPosition; // 드래그 시작 위치, 중간에 놓칠 경우 원복위치. 부모 위치로 고정해야함
 
     // 이미지 드래그 중 변경할 부모 RacTransform
-    [SerializeField] Transform onDragParent;
+    Transform onDragParent;
 
     [SerializeField] public Transform startParent; // 슬롯이 아닌 다른 오브젝트에 드랍시 원복할 부모
+
+
+    private void Start()
+    {
+        filmImage = GetComponent<Image>();
+    }
 
     // 드래그 시작 시 호출되는 함수
     public void OnBeginDrag(PointerEventData eventData)
     {
         beingDraggImg = gameObject;
-        filmImage = GetComponent<Image>();
         if (filmImage != null)
         {
             filmImage.raycastTarget = false;
@@ -83,17 +89,18 @@ public class Film : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
         Debug.Log("드래그 종료");
     }
 
-    public void OnClick()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        // beingDraggImg가 null이 아니고 자기 자신일 때만 동작
-        if (beingDraggImg == null || beingDraggImg != gameObject)
+        // 클릭하면 해당 이미지 확대 => 다시 누르면 원래 자리에 원래 크기로
+        // 혹은 클릭 시 DisplayImage로 확대된 이미지 표시
+        if (filmPanel != null && filmImage != null)
         {
-            Debug.LogWarning("beingDraggImg가 null이거나 자기 자신이 아닙니다. OnClick 동작을 건너뜁니다.");
-            return;
+            filmPanel.gameObject.SetActive(true);
+            filmPanel.panelImage.sprite = filmImage.sprite; // 패널 이미지에 드래그된 이미지의 스프라이트 설정
         }
-        transform.SetParent(startParent);
-        transform.position = startPosition;
-        beingDraggImg = null;
-        Debug.Log("이미지 클릭: Film 칸으로 되돌림");
-    }   
+        else
+        {
+            Debug.Log($" FilmPanel이 비활성화 상태이거나, filmImage가 null입니다. FilmPanel: {filmPanel}, filmImage: {filmImage}");
+        }
+    }
 }
